6442de05ed8f639a4d6de42592223289cf09489d
nicolalandro <nicolaxx94@Live.it>
2022-05-05 17:26:38 +0200

    create demo

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# yolov5_streamlit
+# Yolov5 Streamlit
+This codebase create a realtime web demo with streamlit.
+
+## Develop
+```
+python3.8 -m venv venv
+source venv/bin/activate
+# or on fish shell
+source venv/bin/activate.fish
+
+pip install -r requirements.txt
+
+python -m streamlit run app.py
+```
+
+# References
+* [streamlit](https://streamlit.io/): library for web demo
+* [streamlit_webrtc](https://github.com/whitphx/streamlit-webrtc): to use webcam from browser on stramlit
+* [yolov5](https://github.com/ultralytics/yolov5): trained model
---
+++ streamlit_app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from PIL import Image
+import torch
+import numpy as np
+
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
+import av
+
+
+device = 'cpu'
+if not hasattr(st, 'classifier'):
+    st.model = torch.hub.load('ultralytics/yolov5', 'yolov5s',  _verbose=False)
+    # st.model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5s.pt', _verbose=False)
+    
+
+
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
+
+
+class VideoProcessor:
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        
+        # vision processing
+        flipped = img[:, ::-1, :]
+
+        # model processing
+        im_pil = Image.fromarray(flipped)
+        results = st.model(im_pil)
+        bbox_img = np.array(results.render()[0])
+
+        return av.VideoFrame.from_ndarray(bbox_img, format="bgr24")
+
+
+webrtc_ctx = webrtc_streamer(
+    key="WYH",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration=RTC_CONFIGURATION,
+    video_processor_factory=VideoProcessor,
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=False,
+)
